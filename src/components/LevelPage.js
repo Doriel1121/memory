b673d24fe0f47045b49d716643c7fc667f5bee24
Doriel aboya <doriel494@gmail.com>
2021-02-23 18:20:30 +0200

    all

--- src/components/LevelPage.js
+++ src/components/LevelPage.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from "react";
 import { Link, BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class LevelPage extends PureComponent {
   constructor(props) {
@@ -7,46 +8,57 @@ class LevelPage extends PureComponent {
 
     this.state = {};
   }
+
   componentDidMount = () => {};
 
   render() {
     return (
       <div className="mainDiv">
-        <div className="difficulty">
-          <h1 style={{ color: "#3bd6a4", fontWeight: "900" }}>Memory Game</h1>
+        <div className="container">
+          <div className="row">
+            <div className="col-2"></div>
+            <div className="difficulty col-8">
+              <div>
+                <h1 style={{ color: "#3bd6a4", fontWeight: "900" }}>
+                  Memory Game
+                </h1>
 
-          <h2 style={{ color: "#3bd6a4" }}>Difficulty</h2>
-          <Link
-            to={{
-              pathname: "/game",
-              state: {
-                amount: 5,
-              },
-            }}
-          >
-            <button>EASY</button>
-          </Link>
+                <h2 style={{ color: "#3bd6a4" }}>Difficulty</h2>
+                <Link
+                  to={{
+                    pathname: "/game",
+                    state: {
+                      amount: 5,
+                    },
+                  }}
+                >
+                  <button>EASY</button>
+                </Link>
 
-          <Link
-            to={{
-              pathname: "/game",
-              state: {
-                amount: 8,
-              },
-            }}
-          >
-            <button>MEDIUM</button>
-          </Link>
-          <Link
-            to={{
-              pathname: "/game",
-              state: {
-                amount: 11,
-              },
-            }}
-          >
-            <button>HARD</button>
-          </Link>
+                <Link
+                  to={{
+                    pathname: "/game",
+                    state: {
+                      amount: 8,
+                    },
+                  }}
+                >
+                  <button>MEDIUM</button>
+                </Link>
+                <Link
+                  to={{
+                    pathname: "/game",
+                    state: {
+                      amount: 11,
+                    },
+                  }}
+                >
+                  <button>HARD</button>
+                </Link>
+              </div>
+            </div>
+            <div className="col-2"></div>
+          </div>
         </div>
       </div>
     );
